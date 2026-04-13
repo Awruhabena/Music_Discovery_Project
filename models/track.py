26c74d7f1df58b5786338e58d4432e5afe9b5d9b
Awruhabena <lilybampoe63@gmail.com>
2026-04-13 11:15:57 +0000

    Tracks SQLALCHEMY File

---
+++ models/track.py
@@ -0,0 +1,37 @@
+from sqlalchemy import Column , String , Integer , Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from typing import Optional
+from pydantic import BaseModel # A validator that checks data to and from APIs
+
+from utils.uuid_generator import generate_uuid
+from models.base import Base
+
+class Track(Base):
+    __tablename__ = 'tracks'
+    id = Column(String, primary_key=True, default = generate_uuid)
+    artist_id = Column(String, ForeignKey("artists.id"), nullable=False)
+    spotify_id = Column(String, unique=True, index=True)
+    name = Column(String, nullable=False)
+    release_date = Column(String)
+    album_name = Column(String)
+    duration_ms = Column(Integer)
+    popularity = Column(Integer)
+    preview_url = Column(String)
+    spotify_url = Column(String)
+    track_number = Column(Integer)
+    is_explicit = Column(Boolean, default=False)
+    
+    artist = relationship("Artist", back_populates="tracks")
+    audio_features = relationship("AudioFeature" , back_populates = "track")
+
+class TrackResponse(BaseModel):
+    id: str
+    name: str
+    spotify_id: str
+    artist_id: str
+    album_name: Optional[str]
+    duration_ms: Optional[int]
+    is_explicit: bool
+
+    class Config:
+        from_attributes = True
